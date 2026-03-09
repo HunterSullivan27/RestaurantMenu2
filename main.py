e27bdb42be4098c21e985f41b9850a965b9cf9b7
HunterSullivan27 <hunterdsullivan59@gmail.com>
2026-03-09 15:28:18 +0000

    3.1.3 - adds dressing selection

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 print("Hello! Welcome to Sullivan's Salads.")
+protein_price = 0
+drink_price = 0
 protein_order_complete = False
 while not protein_order_complete:
-    protein_choice = input("What kind of protein would you like in your salad? Chicken, beef, or tofu? Please make sure to answer in all lowercase.")
+    protein_choice = input("What kind of protein would you like in your salad? Chicken, steak, or tofu? Please make sure to answer in all lowercase.")
     if protein_choice == "chicken":
         protein_price = 5.99
         print("Great! That will be $5.99")
         protein_order_complete = True
-    elif protein_choice == "beef":
+    elif protein_choice == "steak":
         protein_price = 7.99
         print("Great! That will be $7.99")
         protein_order_complete = True
@@ -16,4 +18,83 @@ while not protein_order_complete:
         protein_order_complete = True
     else:
         print("Sorry! There was an error with your input. Check to make sure you selected a valid option and answered in all lowercase.")
-protein_order_complete = True
+dressing_order_complete = False
+while not dressing_order_complete:
+    dressing_yes_or_no = input("Would you like dressing on that?")
+    if dressing_yes_or_no == "yes":
+        dressing_choice = input("What kind of dressing? We have ranch, balsamic, and italian.")
+        if dressing_choice == "ranch":
+            dressing_price = 0.5
+            print("Good choice! That'll be 2 nickels.")
+            dressing_order_complete = True
+        elif dressing_choice == "balsamic":
+            dressing_price = 0.25
+            print("My favorite! That'll be an additional 25 cents.")
+            dressing_order_complete = True
+        elif dressing_choice == "italian":
+            dressing_price = 1
+            print("Okay! That'll be $1.")
+            dressing_order_complete = True
+        else:
+            print("Sorry! There was an error with your input. Check to make sure you selected a valid option and answered in all lowercase.")
+    elif dressing_yes_or_no == "no":
+        dressing_price = 0
+        print("Too bad, so sad!")
+        dressing_order_complete = True
+        dressing_choice = "no"
+    else:
+        print("Sorry! There was an error with your input. Check to make sure you selected a valid option and answered in all lowercase.")
+drink_order_complete = False
+while not drink_order_complete:
+    drink_yes_or_no = input("Would you like a drink? (yes or no)")    
+    if drink_yes_or_no == "yes":
+        drink_choice = input("What kind of drink would you like? We have water, Coke Zero, and coffee. Please make sure to answer in all lowercase.")
+        if drink_choice == "water":
+            drink_price = 0
+            print("Great! That will be complimentary")
+            drink_order_complete = True
+        elif drink_choice == "coke zero":
+            drink_price = 2.00
+            print("Great! That will be $2")
+            drink_order_complete = True
+        elif drink_choice == "coffee":
+            drink_price = 4.00
+            print("Great! That will be $4")
+            drink_order_complete = True
+        else:
+            print("Sorry! There was an error with your input. Check to make sure you selected a valid option and answered in all lowercase.")
+    elif drink_yes_or_no == "no":
+        drink_price = 0
+        print("Your loss!")
+        drink_order_complete = True
+        drink_choice = "nothing"
+    else:
+        print("Sorry! There was an error with your input. Check to make sure you selected a valid option and answered in all lowercase.")
+crouton_count_complete = False
+while not crouton_count_complete:
+    crouton_yes_or_no = input("Would you like croutons?")
+    if crouton_yes_or_no == "yes":
+        crouton_count = int(input("Great! How many would you like? It's 25 cents per crouton.").strip())
+        if crouton_count > 10:
+            print("Sorry! We can't give you anymore than 10.")
+            crouton_count = 10
+            crouton_count_complete = True
+        elif crouton_count <= 10:
+            print("Okay great!")
+            crouton_count_complete = True
+        else:
+            print("Sorry! There was an error with your input. Check to make sure you selected a valid option and answered in all lowercase.")
+    elif crouton_yes_or_no == "no":
+        print("Okay!")
+        crouton_count_complete = True
+    else: 
+        print("Sorry! There was an error with your input. Check to make sure you selected a valid option and answered in all lowercase.")
+crouton_price = crouton_count * 0.25
+drink_order_complete = True
+peanut_order_complete = False
+total_cost = protein_price + drink_price + dressing_price + crouton_price
+result = f"Your final order is a {protein_choice} salad with {dressing_choice} dressing, and {drink_choice} to drink. Along with {crouton_count} croutons."
+print(result)
+subtotal = f"Subtotal: ${total_cost}"
+print(subtotal)
+print(f"Total Price: ${total_cost * 1.08:.2f}")
---
+++ plan.md
@@ -0,0 +1,52 @@
+Iteration 1
+
+
+ Ask the user for a type of salad (chicken $5.99, steak $7.99, tofu $6.99).
+
+
+Have the program output the user’s salad selection to verify that the program is working correctly.
+
+Iteration 2
+
+
+Ask the user whether they would like a dressing (yes, no).
+
+
+If they say yes, ask what kind of dressing they would like:
+ranch $0.50
+balsamic $0.25
+italian $1.00
+
+
+Have the program output the user’s dressing selection, or lack of selection, to verify that the program is working correctly.
+
+
+Iteration 3
+
+
+Ask the user whether they would like a beverage (yes, no).
+
+
+If they say yes, ask what kind of beverage they would like:
+water $0.00
+Coke Zero $2.00
+coffee $4.00
+
+
+Have the program output the user’s beverage selection to verify that the program is working correctly.
+
+
+Adjust the program so the total cost only outputs to the user after their sandwich, drink, and fries selection.
+
+Iteration 4
+
+
+Ask the user how many croutons they would like (enter a positive integer; cost is $0.25 per crouton).
+
+After ordering the salad, drink, dressing, and croutons:
+
+
+The program informs the user of their menu selections, for only the items they ordered.
+
+
+The program should print the total cost of the order. Remove any other totals before this point.
