9b3d0ec55ee4e968003c7b2f7652e911d3d7ea75
HunterSullivan27 <hunterdsullivan59@gmail.com>
2026-03-23 16:09:21 +0000

    Partially refactors to list-based menu order tracking

--- main.py
+++ main.py
@@ -1,52 +1,57 @@
 print("Hello! Welcome to Sullivan's Salads.")
-protein_price = 0
 drink_price = 0
-order = []
+order_choices = []
+order_prices = []
+
+# Protein
 protein_order_complete = False
 while not protein_order_complete:
     protein_choice = input("What kind of protein would you like in your salad? Chicken, steak, or tofu? Please make sure to answer in all lowercase.")
     if protein_choice == "chicken":
-        protein_price = 5.99
-        print("Great! That will be $5.99")
+        order_prices.append(5.99)
         protein_order_complete = True
     elif protein_choice == "steak":
-        protein_price = 7.99
-        print("Great! That will be $7.99")
+        order_prices.append(7.99)
         protein_order_complete = True
     elif protein_choice == "tofu":
-        protein_price = 6.99
-        print("Great! That will be $6.99")
+        order_prices.append( 6.99)
         protein_order_complete = True
     else:
         print("Sorry! There was an error with your input. Check to make sure you selected a valid option and answered in all lowercase.")
-order.append(protein_choice)
+print(f"Great! That will be {order_prices[-1]}")
+order_choices.append(protein_choice)
+
+# Dressing
 dressing_order_complete = False
 while not dressing_order_complete:
     dressing_yes_or_no = input("Would you like dressing on that?")
     if dressing_yes_or_no == "yes":
         dressing_choice = input("What kind of dressing? We have ranch, balsamic, and italian.")
         if dressing_choice == "ranch":
-            dressing_price = 0.5
+            order_prices.append( 0.5)
             print("Good choice! That'll be 2 nickels.")
             dressing_order_complete = True
         elif dressing_choice == "balsamic":
-            dressing_price = 0.25
+            order_prices.append( 0.25)
             print("My favorite! That'll be an additional 25 cents.")
             dressing_order_complete = True
         elif dressing_choice == "italian":
-            dressing_price = 1
+            order_prices.append( 1)
             print("Okay! That'll be $1.")
             dressing_order_complete = True
         else:
             print("Sorry! There was an error with your input. Check to make sure you selected a valid option and answered in all lowercase.")
     elif dressing_yes_or_no == "no":
-        dressing_price = 0
+        order_prices.append(0)
         print("Too bad, so sad!")
         dressing_order_complete = True
         dressing_choice = "no"
     else:
         print("Sorry! There was an error with your input. Check to make sure you selected a valid option and answered in all lowercase.")
-order.append(dressing_choice)
+order_choices.append(dressing_choice)
+
+# Drink
+# TODO: migrate to order_prices.append(???) instead of drink_price = ???
 drink_order_complete = False
 while not drink_order_complete:
     drink_yes_or_no = input("Would you like a drink? (yes or no)")    
@@ -73,7 +78,9 @@ while not drink_order_complete:
         drink_choice = "nothing"
     else:
         print("Sorry! There was an error with your input. Check to make sure you selected a valid option and answered in all lowercase.")
-order.append(drink_choice)
+order_choices.append(drink_choice)
+
+# Croutons
 crouton_count_complete = False
 crouton_count = 0
 while not crouton_count_complete:
@@ -95,11 +102,17 @@ while not crouton_count_complete:
         crouton_count_complete = True
     else: 
         print("Sorry! There was an error with your input. Check to make sure you selected a valid option and answered in all lowercase.")
-crouton_price = crouton_count * 0.25
-order.append(crouton_count)
+order_choices.append(crouton_count)
+order_prices.append(crouton_count * 0.25)
 drink_order_complete = True
-total_cost = protein_price + drink_price + dressing_price + crouton_price
-result = f"Your final order is a {protein_choice} salad with {dressing_choice} dressing, and {drink_choice} to drink. Along with {crouton_count} croutons."
+
+total_cost = 0
+for price in order_prices:
+    total_cost += price
+
+# TODO: use the order_choices list to access each salad option...
+
+result = f"Your final order is a {order_choices[0]} salad with {dressing_choice} dressing, and {drink_choice} to drink. Along with {crouton_count} croutons."
 print(result)
 subtotal = f"Subtotal: ${total_cost}"
 print(subtotal)
